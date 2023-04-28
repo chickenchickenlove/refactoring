55eec27ff3367ad53774534b9e071257748634eb
chickenchickenlove <ojt90902@naver.com>
2023-04-28 23:35:16 +0900

    냄새1 공부

---
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_01_change_method_declaration/after/StudyDashboard.java
@@ -0,0 +1,49 @@
+package me.whiteship.refactoring._01_smell_mysterious_name._01_change_method_declaration;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// studyReviews() 메서드는 중의적인 의미를 포함한다.
+// 1. 리뷰를 스터디하는 메서드일지도
+// 2. 스터디 리뷰를 읽어온다는 메서드일지도
+// 함수의 이름을 좀 더 명확하게 수정해주는 것이 좋다.
+public class StudyDashboard {
+
+    private Set<String> usernames = new HashSet<>();
+
+    private Set<String> reviews = new HashSet<>();
+
+    private void studyReviews(GHIssue issue) throws IOException {
+        List<GHIssueComment> comments = issue.getComments();
+        for (GHIssueComment comment : comments) {
+            usernames.add(comment.getUserName());
+            reviews.add(comment.getBody());
+        }
+    }
+
+    public Set<String> getUsernames() {
+        return usernames;
+    }
+
+    public Set<String> getReviews() {
+        return reviews;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.studyReviews(issue);
+        studyDashboard.getUsernames().forEach(System.out::println);
+        studyDashboard.getReviews().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/after/StudyReview.java
@@ -0,0 +1,2 @@
+package me.whiteship.refactoring._01_smell_mysterious_name._03_rename_field.after;public class StudyReview {
+}
