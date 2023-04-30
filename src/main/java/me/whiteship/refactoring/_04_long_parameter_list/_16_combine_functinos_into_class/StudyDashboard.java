83dfcb98772912407ee7943656ec893195407bad
chickenchickenlove <ojt90902@naver.com>
2023-04-30 18:54:16 +0900

    refactoring 16 study

---
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_16_combine_functinos_into_class/StudyDashboard.java
@@ -0,0 +1,141 @@
+package me.whiteship.refactoring._04_long_parameter_list._16_combine_functinos_into_class;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StudyDashboard {
+
+    private final int totalNumberOfEvents;
+
+    public StudyDashboard(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        StudyDashboard studyDashboard = new StudyDashboard(15);
+        studyDashboard.print();
+    }
+
+    private void print() throws IOException, InterruptedException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        List<Participant> participants = new CopyOnWriteArrayList<>();
+
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
+
+        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
+            int eventId = index;
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        GHIssue issue = repository.getIssue(eventId);
+                        List<GHIssueComment> comments = issue.getComments();
+
+                        for (GHIssueComment comment : comments) {
+                            String username = comment.getUserName();
+                            boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
+                            Participant participant = null;
+                            if (isNewUser) {
+                                participant = new Participant(username);
+                                participants.add(participant);
+                            } else {
+                                participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+                            }
+
+                            participant.setHomeworkDone(eventId);
+                        }
+
+                        latch.countDown();
+                    } catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            });
+        }
+
+        latch.await();
+        service.shutdown();
+
+        // 이 코드에서 출력을 담당한다.
+        // homework, username, totalNumberOfParticipants 등이 매개변수에 계속 전달되는 것을 볼 수 있다.
+        // homework, username은 Participant라는 객체에서 전달됨.
+        // 전달되는 이 매개변수들은 이 메서드에서만 쓰이므로, 메서드 + 매개변수로 따로 클래스를 뽑는 것이 코드 복잡도를 줄일 수 있음.
+        try (FileWriter fileWriter = new FileWriter("participants.md");
+             PrintWriter writer = new PrintWriter(fileWriter)) {
+            participants.sort(Comparator.comparing(Participant::username));
+
+            writer.print(header(participants.size()));
+
+            participants.forEach(p -> {
+                String markdownForHomework = getMarkdownForParticipant(p.username(), p.homework());
+                writer.print(markdownForHomework);
+            });
+        }
+    }
+
+    private String getMarkdownForParticipant(String username, Map<Integer, Boolean> homework) {
+        return String.format("| %s %s | %.2f%% |\n", username, checkMark(homework), getRate(homework));
+    }
+
+    /**
+     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
+     */
+    private String checkMark(Map<Integer, Boolean> homework) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1 ; i <= this.totalNumberOfEvents ; i++) {
+            if(homework.containsKey(i) && homework.get(i)) {
+                line.append("|:white_check_mark:");
+            } else {
+                line.append("|:x:");
+            }
+        }
+        return line.toString();
+    }
+
+    private double getRate(Map<Integer, Boolean> homework) {
+        long count = homework.values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / this.totalNumberOfEvents);
+    }
+
+    /**
+     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
+     * | --- | --- | --- | --- | --- |
+     */
+    private String header(int totalNumberOfParticipants) {
+        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
+
+        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
+            header.append(String.format(" %d주차 |", index));
+        }
+        header.append(" 참석율 |\n");
+
+        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
+        header.append("|\n");
+
+        return header.toString();
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_16_combine_functinos_into_class/after/StudyPrinter.java
@@ -0,0 +1,86 @@
+package me.whiteship.refactoring._04_long_parameter_list._16_combine_functinos_into_class.after;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+// 사용되는 매개변수를 하나의 클래스로 모으고, 각 메서드들에 필요한 매개변수를 줄인다.
+public class StudyPrinter {
+
+    private final int totalNumberOfEvents;
+    private final List<Participant> participants;
+
+    public StudyPrinter(int totalNumberOfEvents, List<Participant> participants) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+        this.participants = participants;
+    }
+
+
+    public void print() throws IOException {
+        // 이 코드에서 출력을 담당한다.
+        // homework, username, totalNumberOfParticipants 등이 매개변수에 계속 전달되는 것을 볼 수 있다.
+        // homework, username은 Participant라는 객체에서 전달됨.
+        // 전달되는 이 매개변수들은 이 메서드에서만 쓰이므로, 메서드 + 매개변수로 따로 클래스를 뽑는 것이 코드 복잡도를 줄일 수 있음.
+        try (FileWriter fileWriter = new FileWriter("participants.md");
+             PrintWriter writer = new PrintWriter(fileWriter)) {
+            this.participants.sort(Comparator.comparing(Participant::username));
+
+            writer.print(header());
+
+            this.participants.forEach(p -> {
+                String markdownForHomework = getMarkdownForParticipant(p);
+                writer.print(markdownForHomework);
+            });
+        }
+    }
+
+    private String getMarkdownForParticipant(Participant participant) {
+        return String.format("| %s %s | %.2f%% |\n",
+                participant.username(),
+                checkMark(participant.homework()),
+                getRate(participant.homework()));
+    }
+
+    /**
+     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
+     */
+    private String checkMark(Map<Integer, Boolean> homework) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1 ; i <= this.totalNumberOfEvents ; i++) {
+            if(homework.containsKey(i) && homework.get(i)) {
+                line.append("|:white_check_mark:");
+            } else {
+                line.append("|:x:");
+            }
+        }
+        return line.toString();
+    }
+
+    private double getRate(Map<Integer, Boolean> homework) {
+        long count = homework.values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / this.totalNumberOfEvents);
+    }
+
+    /**
+     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
+     * | --- | --- | --- | --- | --- |
+     */
+    private String header() {
+        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", this.participants.size()));
+
+        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
+            header.append(String.format(" %d주차 |", index));
+        }
+        header.append(" 참석율 |\n");
+
+        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
+        header.append("|\n");
+
+        return header.toString();
+    }
+}
